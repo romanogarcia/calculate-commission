1300f95a8cb2844b79ccc6c3e1ba3599003f8bf0
Roman Garcia <romanogarcia.ph@gmail.com>
2019-09-13 16:33:09 +0800

    Commit Unit Testing

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit9d9553a89e510c6a3195d3c8ec3fa0d3
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit9d9553a89e510c6a3195d3c8ec3fa0d3::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire9d9553a89e510c6a3195d3c8ec3fa0d3($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire9d9553a89e510c6a3195d3c8ec3fa0d3($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
